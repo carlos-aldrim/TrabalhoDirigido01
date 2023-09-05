596c2c8fc94d1392d55c1d5dca05fde2e30a34b6
Carlos Aldrim <carlosaldrimfilho@gmail.com>
2023-09-05 10:29:04 -0300

    Atualizando projeto

--- src/views/pages/ToDoList/ToDoList.tsx
+++ src/views/pages/ToDoList/ToDoList.tsx
@@ -1,36 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
 } from "react-native";
 import { styles } from "./ToDoList.styles";
 import WebView from "react-native-webview";
 
-interface Task {
-  id: number;
-  text: string;
-}
-
 export const ToDoList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleAddTask = () => {
-    if (newTask.trim()) {
-      setTasks((prevTasks) => [
-        ...prevTasks,
-        { id: Date.now(), text: newTask },
-      ]);
-      setNewTask("");
-    }
-  };
-
-  const handleRemoveTask = (taskId: number) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-  };
 
   return (
     <View style={styles.container}>
